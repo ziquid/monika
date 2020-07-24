8ba4d006266c80555538264546b9fa32d5f359ec
Joseph Cheek <joseph@ziquid.com>
2020-07-24 10:43:36 -0400

    updated build script to do a db dump and another drush cr

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,10 @@ set -xe
 git co master
 git pull
 composer.phar install
+drush cr || :
 drush updb -y
 drush cr || :
 [ $(uname) == Linux ] && drush cim -y sync || drush cim -y sync --partial
 drush cr
 drush cc views
-# [ $(uname) == Linux ] && drush pmu -y stage_file_proxy
-# [ $(uname) == Linux ] && $(dirname "$0")/db-dump.sh
+$(dirname "$0")/db-dump.sh
